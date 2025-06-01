5ba91b09c9d70e8cc7394c18e0390cb8a88b7d17
Grindeliz Altuve <grindeliz@gmail.com>
2025-05-31 22:08:54 -0400

    Fixes: several fixes in model folder

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user_model import User
+from .role_model import Role
+from .level_model import Level
+from .question_model import Question
+from .answer_model import Answer
+from .trivia_model import Trivia
